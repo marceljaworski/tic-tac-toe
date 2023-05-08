15f96e79a71b9768bea408e7590eda4d1e8bfba6
Marcelearn <jaworskimarcel@gmail.com>
2023-05-08 09:49:42 +0200

    ng g c board

---
+++ src/app/board/board.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-board',
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.scss']
+})
+export class BoardComponent {
+
+  squares: any[];
+  xIsNext: boolean;
+  winner: string;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.newGame();
+  }
+
+  newGame() {
+    this.squares = Array(9).fill(null);
+    this.winner = null;
+    this.xIsNext = true;
+  }
+
+  get player() {
+    return this.xIsNext ? 'X' : 'O';
+  }
+
+  
+
+}
--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class SquareComponent {
 
+  rando: number = Math.random();
+  // constructor() {
+  //   setInterval(() => this.rando = Math.random(), 500);
+  // }
+
+
 }
